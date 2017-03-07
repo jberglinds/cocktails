ce435bdd490386aca3497d9f1fee58d9c7f6bc3f
Jonathan Berglind <jonathan.berglind@gmail.com>
2017-03-07 22:24:57 +0100

    Add some more base views

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,49 @@
-let app = angular.module('cocktails', ['ui.router', 'controllers']);
+let app = angular.module('cocktails', ['ui.router', 'controllers', 'angular-loading-bar']);
 
-app.config(function($stateProvider) {
-  let welcome = {
-    name: 'welcome',
-    url: '',
-    templateUrl: 'views/welcome.html'
-  }
+app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = false;
+}]);
 
-  let drinks = {
-    name: 'drinks',
-    url: '/drinks',
-    templateUrl: 'views/drinks.html',
-    controller: 'DrinksController'
-  }
+app.config(function($stateProvider, $urlRouterProvider) {
+    let welcome = {
+        name: 'welcome',
+        url: '/',
+        templateUrl: 'views/welcome.html'
+    }
 
-  $stateProvider.state(welcome);
-  $stateProvider.state(drinks);
+    let drinks = {
+        name: 'drinks',
+        url: '/drinks',
+        templateUrl: 'views/drinks.html',
+        controller: 'DrinksController'
+    }
+
+    let spirits = {
+        name: 'spirits',
+        url: '/spirits',
+        templateUrl: 'views/spirits.html',
+        controller: 'SpiritsController'
+    }
+
+    let mixers = {
+        name: 'mixers',
+        url: '/mixers',
+        templateUrl: 'views/mixers.html',
+        controller: 'MixersController'
+    }
+
+    let events = {
+        name: 'events',
+        url: '/events',
+        templateUrl: 'views/events.html',
+        controller: 'EventsController'
+    }
+
+    $stateProvider.state(welcome);
+    $stateProvider.state(drinks);
+    $stateProvider.state(spirits);
+    $stateProvider.state(mixers);
+    $stateProvider.state(events);
+
+    $urlRouterProvider.otherwise('/')
 });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,3 +5,21 @@ controllerModule.controller('DrinksController', ['$scope',
 		$scope.test = 'test';
 	}
 ]);
+
+controllerModule.controller('SpiritsController', ['$scope',
+	function($scope) {
+		$scope.test = 'test';
+	}
+]);
+
+controllerModule.controller('MixersController', ['$scope',
+	function($scope) {
+		$scope.test = 'test';
+	}
+]);
+
+controllerModule.controller('EventsController', ['$scope',
+	function($scope) {
+		$scope.date = new Date();
+	}
+]);
