e1c08a5bd23eab5392ef70f4f5dd000a017413ba
Jonathan Berglind <jonathan.berglind@gmail.com>
2017-03-09 22:10:54 +0100

    Wire up controllers to views with some mock data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,28 @@ let controllerModule = angular.module('controllers', []);
 controllerModule.controller('DrinksController', ['$scope',
 	function($scope) {
 
+		$scope.drinks = [
+			{
+				id: 1234,
+				name: 'Lolly',
+				image: 'http://placeholder.pics/svg/200/FF9100-FF0093/FFFFFF-000000/Lolly',
+				tags: [
+					'Vodka',
+					'Rum',
+					'Tequila',
+					'Gin'
+				]
+			},
+			{
+				id: 1234,
+				name: 'GT',
+				image: 'http://placeholder.pics/svg/200/FF9100-FF0093/FFFFFF-000000/GT',
+				tags: [
+					'Gin',
+				]
+			},
+		]
+
 		$scope.noOfIngredients = 1;
 		$scope.range = function(n) {
         	return new Array(n);
@@ -12,18 +34,75 @@ controllerModule.controller('DrinksController', ['$scope',
 
 controllerModule.controller('SpiritsController', ['$scope',
 	function($scope) {
-		$scope.test = 'test';
+
+		$scope.spirits = [
+			{
+				id: 1234,
+				name: 'Dragon Blood',
+				type: 'Liqueur',
+				abv: 50
+			},
+			{
+				id: 1234,
+				name: 'Bombay Sapphire London Dry Gin',
+				type: 'Gin',
+				abv: 40
+			},
+		]
+
 	}
 ]);
 
 controllerModule.controller('MixersController', ['$scope',
 	function($scope) {
-		$scope.test = 'test';
+
+		$scope.mixers = [
+			{
+				id: 1234,
+				name: 'Orange Juice',
+				type: 'Juice',
+			},
+			{
+				id: 1234,
+				name: 'Milk',
+				type: 'Dairy',
+			},
+			{
+				id: 1234,
+				name: 'Lemon Slice',
+				type: 'Fruit',
+			},
+		]
+
 	}
 ]);
 
 controllerModule.controller('EventsController', ['$scope',
 	function($scope) {
-		$scope.date = new Date();
+
+		let date = new Date();
+		date.setDate(date.getDate() + 6);
+
+		let date2 = new Date();
+		date2.setDate(date2.getDate() + 10);
+
+		$scope.events = [
+			{
+				id: 1234,
+				name: 'Onsdagspub',
+				date: new Date(),
+			},
+			{
+				id: 1234,
+				name: 'Torsdagspub',
+				date: date,
+			},
+			{
+				id: 1234,
+				name: 'Mottagningstack',
+				date: date2,
+			},
+		]
+
 	}
 ]);
