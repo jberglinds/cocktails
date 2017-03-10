815b8b20c7996f4594c3af2bc59ea4ecfcd6a9c2
Albin Remnestål <albin.remnestal@gmail.com>
2017-03-10 18:35:38 +0100

    Adding GET api for /spirits
    Returns id, name and abv of all entries in the spririts table

--- app/api-router.js
+++ app/api-router.js
@@ -1,11 +1,22 @@
 "use strict";
 
 let express = require('express');
+let mysql = require('mysql')
 
 let router = express.Router();
 
 router.get('/test', function(req, res) {
 	res.send('It works!');
+})
+
+router.get('/spirits', function(req, res) {
+	let connection = mysql.createConnection(require('../database/database_credentials.json'));
+	connection.connect()
+	connection.query('SELECT id, name, abv FROM spirits', function (err, rows, fields) {
+		if (err) throw err
+		res.send(rows);
+	})
+	connection.end()
 });
 
 module.exports = router;
