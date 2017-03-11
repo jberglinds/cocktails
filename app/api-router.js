796791e57d5d640100d185250a6f140408392a4d
Albin Remnestål <albin.remnestal@gmail.com>
2017-03-11 22:28:36 +0100

    Adding api end-point for mixers / mixer

--- app/api-router.js
+++ app/api-router.js
@@ -9,6 +9,11 @@ router.get('/test', function(req, res) {
 	res.send('It works!');
 })
 
+/*
+* GET /spirits
+* Returns all spirits from the db, sorted asc by name
+* id, name, abv, type
+*/
 router.get('/spirits', function(req, res) {
 	let connection = mysql.createConnection(require('../database/database_credentials.json'));
 	let query = `
@@ -26,6 +31,11 @@ router.get('/spirits', function(req, res) {
 	connection.end()
 })
 
+/*
+* GET /spirit/spiritId
+* Returns the spirit that matches spiritId from the db
+* name, abv, description, type
+*/
 router.get('/spirit/:spiritId(\\d+)', function(req, res) {
 	let connection = mysql.createConnection(require('../database/database_credentials.json'));
 	let query = `
@@ -34,6 +44,46 @@ router.get('/spirit/:spiritId(\\d+)', function(req, res) {
 		JOIN base_spirits
 		ON spirits.type_of_liqour=base_spirits.id
 		WHERE spirits.id=${req.params.spiritId};
+	`
+	connection.connect()
+	connection.query(query, function (err, rows, fields) {
+		if (err) throw err
+		res.send(rows);
+	})
+	connection.end()
+});
+
+/*
+* GET /mixers
+* Returns all mixers from the db, sorted asc by name
+* id, name
+*/
+router.get('/mixers', function(req, res) {
+	let connection = mysql.createConnection(require('../database/database_credentials.json'));
+	let query = `
+		SELECT mixers.id, mixers.name
+		FROM mixers
+		ORDER BY mixers.name ASC;
+	`
+	connection.connect()
+	connection.query(query, function (err, rows, fields) {
+		if (err) throw err
+		res.send(rows);
+	})
+	connection.end()
+})
+
+/*
+* GET /mixer/mixerId
+* Returns the mixer that matches mixerId from the db
+* name, description
+*/
+router.get('/mixer/:mixerId(\\d+)', function(req, res) {
+	let connection = mysql.createConnection(require('../database/database_credentials.json'));
+	let query = `
+		SELECT mixers.name,  mixers.description
+		FROM mixers
+		WHERE mixers.id=${req.params.mixerId};
 	`
 	connection.connect()
 	connection.query(query, function (err, rows, fields) {
