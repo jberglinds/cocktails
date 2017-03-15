cbe17844d132d2fdab48e5416337093ef647b100
Albin Remnestål <albin.remnestal@gmail.com>
2017-03-15 18:23:55 +0100

    Refactoring debugging statements as middleware

--- app/api-router.js
+++ app/api-router.js
@@ -7,6 +7,23 @@ let express = require('express');
 let router = express.Router();
 
 /*
+* Log all incoming requests
+*/
+router.use('/', function(req, res, next) {
+    switch (req.method) {
+        case 'GET':
+            console.log('\x1b[32m\x1b[1m'+ req.method + '\x1b[0m request to \x1b[4m' + req.path + '\x1b[0m');
+            break;
+        case 'POST':
+            console.log('\x1b[33m\x1b[1m'+ req.method + '\x1b[0m request to \x1b[4m' + req.path + '\x1b[0m');
+            break;
+        default:
+            console.log('\x1b[31m\x1b[1m'+ req.method + '\x1b[0m request to \x1b[4m' + req.path + '\x1b[0m');
+    }
+    next();
+});
+
+/*
 * Passphrase authorization for all requests to /events/:eventId
 */
 router.use('/events/:eventId', function(req, res, next) {
