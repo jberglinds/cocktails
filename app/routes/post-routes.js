c135e93207516c7883d5a20131da20e2f628227c
Albin Remnestål <albin.remnestal@gmail.com>
2017-03-16 09:35:09 +0100

    Adding api end-point for POST /drinks/add
    Also adding node package 'synchronized' to project

--- app/routes/post-routes.js
+++ app/routes/post-routes.js
@@ -3,6 +3,11 @@
 let mysql = require('mysql');
 let database_credentials = require('../../database/database_credentials.json');
 
+let sync = require('synchronize');
+let fiber = sync.fiber;
+let wait = sync.await;
+let defer = sync.defer;
+
 /*
 * API end-points for POST requests
 */
@@ -190,6 +195,152 @@ module.exports = function(router) {
         }
     });
 
+    /*
+    * POST /drinks/add
+    * name, description, image_url, spirits, mixers, instructions
+    */
+    router.post('/drinks/add', function(req, res) {
+        if (req.body.name === undefined || req.body.description === undefined ||
+            req.body.image_url === undefined || req.body.spirits === undefined ||
+            req.body.mixers === undefined || req.body.instructions === undefined){
+            res.sendStatus(400);
+        } else {
+
+            fiber(function() {
+                let instructions = req.body.instructions;
+                let spirits = [];
+                for (let i = 0; i < req.body.spirits.length; i++) {
+                    let obj = {};
+                    obj.amount = req.body.spirits[i].amount;
+                    if (req.body.spirits[i].id < 1000) {
+                        // Base spirit
+                        function add_base_spirit_data(obj, defer) {
+                            let connection = mysql.createConnection(database_credentials);
+                            let query = `
+                                SELECT base_spirits.name
+                                FROM base_spirits
+                                WHERE base_spirits.id=${req.body.spirits[i].id};
+                            `;
+                            connection.connect();
+                            connection.query(query, function (err, rows, fields) {
+                                if (err) {
+                                    console.log(err_print(req.path));
+                                    console.log(err);
+                                    obj.base_spirit = {id: -1, name: "Error"};
+                                } else {
+                                    if (rows.length === 0) {
+                                        obj.base_spirit = {id: req.body.spirits[i].id, name: "Unknown"};
+                                    } else {
+                                        obj.base_spirit = {id: req.body.spirits[i].id, name: rows[0].name};
+                                    }
+                                    defer();
+                                }
+                            });
+                            connection.end();
+                        }
+                        // Synchronous mysql query for base_spirits
+                        wait(add_base_spirit_data(obj, defer()));
+                        spirits.push(obj);
+
+                    } else {
+                        // Spirit
+                        function add_spirit_data(obj, defer) {
+                            let connection = mysql.createConnection(database_credentials);
+                            let query = `
+                                SELECT spirits.name, base_spirits.id AS ba_id , base_spirits.name AS ba_name
+                                FROM spirits
+                                JOIN base_spirits
+                                ON spirits.type_of_liqour=base_spirits.id
+                                WHERE spirits.id=${req.body.spirits[i].id};
+                            `;
+                            connection.connect();
+                            connection.query(query, function (err, rows, fields) {
+                                if (err) {
+                                    console.log(err_print(req.path));
+                                    console.log(err);
+                                    obj.spirit = {id: -1, name: "Error"};
+                                } else {
+                                    if (rows.length === 0) {
+                                        obj.base_spirit = {id: -1, name: "Unknown"};
+                                        obj.spirit = {id: req.body.spirits[i].id, name: "Unknown"};
+                                    } else {
+                                        obj.base_spirit = {id: rows[0].ba_id, name: rows[0].ba_name};
+                                        obj.spirit = {id: req.body.spirits[i].id, name: rows[0].name};
+                                    }
+                                    defer();
+                                }
+                            });
+                            connection.end();
+                        }
+                        // Synchronous mysql query for spirits
+                        wait(add_spirit_data(obj, defer()));
+                        spirits.push(obj);
+                    }
+                }
+
+                let mixers = [];
+                for (let i = 0; i < req.body.mixers.length; i++) {
+                    let obj = {};
+                    obj.amount = req.body.mixers[i].amount;
+
+                    function add_mixer_data(obj, defer) {
+                        let connection = mysql.createConnection(database_credentials);
+                        let query = `
+                            SELECT mixers.name
+                            FROM mixers
+                            WHERE mixers.id=${req.body.mixers[i].id};
+                        `;
+                        connection.connect();
+                        connection.query(query, function (err, rows, fields) {
+                            if (err) {
+                                console.log(err_print(req.path));
+                                console.log(err);
+                                obj.mixer = {id: -1, name: "Error"};
+                            } else {
+                                if (rows.length === 0) {
+                                    obj.mixer = {id: req.body.mixers[i].id, name: "Unknown"};
+                                } else {
+                                    obj.mixer = {id: req.body.mixers[i].id, name: rows[0].name};
+                                }
+                                defer();
+                            }
+                        });
+                        connection.end();
+                    }
+                    // Synchronous mysql query for mixers
+                    wait(add_mixer_data(obj, defer()));
+                    mixers.push(obj);
+                }
+
+                // Add the constructed drink to the database
+                let connection = mysql.createConnection(database_credentials);
+                let query = `
+                    INSERT IGNORE INTO drinks
+                        (name, description, image_url, howto_json, spirits_json, mixers_json)
+                    VALUES
+                        (
+                            "${req.body.name}",
+                            "${req.body.description}",
+                            "${req.body.image_url}",
+                            "${JSON.stringify(instructions).replace(new RegExp('"', 'g'), '\\"')}",
+                            "${JSON.stringify(spirits).replace(new RegExp('"', 'g'), '\\"')}",
+                            "${JSON.stringify(mixers).replace(new RegExp('"', 'g'), '\\"')}"
+                        );
+                `;
+                connection.connect();
+                connection.query(query, function (err, rows, fields) {
+                    if (err) {
+                        res.sendStatus(500);
+                        console.log(err_print(req.path));
+                        console.log(err);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                });
+                connection.end();
+            });
+        }
+    });
 
 };
 
